deba1557300f1e380c3e528f66d3a34302df3e37
SeanPrice13 <bentleyprice@live.com>
2022-06-11 04:19:35 -0400

    Updated app.js code.

--- js/app.js
+++ js/app.js
@@ -6,28 +6,27 @@ const rock = document.getElementById('player-section').firstElementChild,
     comChoice = [rock.src, paper.src, scissors.src],
     reloadGame = document.getElementById('reload').addEventListener('click', () => location.reload());
 let wins = 0,
-    losses = 0,
-    ties = 0,
-    round = 1;
+  losses = 0,
+  ties = 0,
+  round = 1;
 
-document.getElementById('round').innerText = `Round ${round}`;
-document.getElementById('status').innerText = 'Pick a hand.';
+document.getElementById("round").innerText = `Round ${round}`;
 
 // Functions
-const playerChoice = (e) => {
+const playerChoice = ({target}) => {
     [rock, paper, scissors].map(item => item.removeEventListener("click", playerChoice));
     let rand = comChoice[Math.floor(Math.random() * comChoice.length)];
     comPlayer.src = rand;
-    const siblings = (ele) => [].slice.call(ele.parentNode.children).filter((child) => child !== ele);
-    siblings(e.target).map(el => el.style.display = 'none');
-    e.target.src == comPlayer.src ? roundTie() : validateRound(e.target.src, comPlayer.src);
+    const siblings = (el) => [].slice.call(el.parentNode.children).filter((child) => child !== el);
+    siblings(target).map(el => el.style.display = 'none');
+    target.src == comPlayer.src ? roundTie() : validateRound(target.src, comPlayer.src);
     newRound();
 };
 
 const roundWin = () => {
     rock.parentElement.setAttribute("id", "win");
     comPlayer.parentElement.setAttribute("id", "lose");
-    document.getElementById("status").innerText = "You Won!";
+    document.getElementById("status").innerText = "Great, You Won!";
     wins++;
 };
 
@@ -64,6 +63,7 @@ const newRound = () => {
         rock.style.display = 'inline';
         scissors.style.display = 'inline';
         comPlayer.src = rock.src;
-    }, 1000);
-} 
+    }, 750);
+}
+
 newRound(); // Nice 😏
